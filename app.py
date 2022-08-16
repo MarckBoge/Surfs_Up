2dade4b2a9ef3bfbf72290b20d32ae5dadf5f68e
MarckBoge <90155651+MarckBoge@users.noreply.github.com>
2022-08-16 12:44:10 -0400

    update Surfs Up

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+@app.route('/product/<name>')
+def get_product(name):
+    return "The product is " + str(name)
+    
